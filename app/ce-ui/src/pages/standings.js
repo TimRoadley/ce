e49f41d4f9bb9975d26c4baf18c511db730f1db6
Tim Roadley <timroadley@icloud.com>
2020-09-04 14:53:25 +1000

    Update standings.js

--- app/ce-ui/src/pages/standings.js
+++ app/ce-ui/src/pages/standings.js
@@ -1,47 +1,125 @@
 import React from "react";
 import "./styles/home.css";
-import { cePlayers } from '../data/db-ceplayer';
+import { cePlayers } from "../data/db-ceplayer";
 import Loading from "../components/loading";
-import ReactTable from 'react-table'
-import 'react-table/react-table.css'
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import Moment from 'react-moment'; // https://www.npmjs.com/package/react-moment
+import 'moment/min/locales';
+import 'moment-timezone';
+import { Link } from 'react-router-dom'
 
 export default class Standings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      loading: true,
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            players: [],
-            loading: true,
-        }
+  componentDidMount() {
+    cePlayers().then((result) => {
+      console.info("ITEMS", result);
+      this.setState({
+        loading: false,
+        players: result.sort(
+          (a, b) =>
+            (b.latest_priority > a.latest_priority) -
+              (b.latest_priority < a.latest_priority) ||
+            (a.uptime > b.uptime) - (a.uptime < b.uptime) ||
+            (a.mac > b.mac) - (a.mac < b.mac)
+        ),
+      });
+    });
+  }
+
+  render() {
+    var view;
+    if (this.state.loading) {
+      view = <Loading />;
+    } else {
+      const columns = [
+        {
+          Header: () => <div style={{ textAlign: "left" }}>Class</div>,
+          accessor: "class",
+          maxWidth: 60,
+          Cell: (props) => (
+            <span>
+              <img
+                src={`/images/IconSmall_${props.original.class}.gif`}
+                alt=""
+              ></img>
+            </span>
+          ),
+        },
+        {
+          Header: () => <div style={{ textAlign: "left" }}>Character</div>,
+          accessor: "name",
+          Cell: (props) => (
+            <span className={props.original.class}>
+              <div><Link className={`${props.original.class}`} to={`/loot/standings/${props.original.name}`}>{props.original.name}</Link></div>
+            </span>
+          ),
+        },
+        {
+            Header: () => <div style={{ textAlign: "left" }}>Guild Rank</div>,
+            accessor: "rank",
+          },
+        {
+          Header: () => <div style={{ textAlign: "left" }}>Loot Priority</div>,
+          accessor: "latest_priority",
+          Cell: (props) => (
+            <span className="artifact">
+              <strong>{props.original.latest_priority}</strong>{" "}
+            </span>
+          ),
+        },
+        {
+          Header: () => <div style={{ textAlign: "left" }}>Effort Points</div>,
+          accessor: "latest_ep",
+          Cell: (props) => (
+            <span className="common">{props.original.latest_ep}</span>
+          ),
+        },
+        {
+          Header: () => <div style={{ textAlign: "left" }}>Gear Points</div>,
+          accessor: "latest_gp",
+          Cell: (props) => (
+            <span className="epic">{props.original.latest_gp}</span>
+          ),
+        },
+        {
+            Header: () => <div style={{ textAlign: "left" }}>Last Updated</div>,
+            accessor: "latest_update",
+            Cell: props => <span><Moment fromNow>{props.original.latest_update*1000}</Moment></span>
+          },
+      ];
+
+      view = (
+        <div>
+          <ReactTable
+            data={this.state.players}
+            columns={columns}
+            //filterable={true}
+            showPagination={false}
+            //pageSizeOptions={[10, 50, 100, data.length]}
+            defaultPageSize={this.state.players.length}
+            //pivotBy={this.state.pivotBy}
+            minRows={0}
+            // defaultFilterMethod={(filter, row) => matchSorter([row[filter.id]], filter.value).length !== 0}
+            className={"standings_table"}
+          />
+        </div>
+      );
     }
 
-    componentDidMount() {
-
-        cePlayers().then(result => {
-
-            console.info("ITEMS", result);
-        });
-
-        this.setState({
-            loading: false
-        })
-    }
-
-    render() {
-        var view;
-        if (this.state.loading) { view = <Loading /> }
-        else {
-            view = <div>
-                Insert table
-            </div>
-        }
-
-
-
-        return <div>
-            <h1>
-                EP/GP Standings
-              </h1>
-              {view}</div>
-    }
+    return (
+      <div>
+        <h1>EP/GP Standings</h1>
+        <p>Pro-tip: Sort by multiple columns by holding shift.</p>
+        {view}
+      </div>
+    );
+  }
 }
